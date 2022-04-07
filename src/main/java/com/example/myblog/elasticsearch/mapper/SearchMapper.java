f788ecc6fdd4438f739acce628399fd215f21ca0
sjcsjc123 <1401189096@qq.com>
2022-04-07 11:23:14 +0800

    update

--- src/main/java/com/example/myblog/elasticsearch/mapper/SearchMapper.java
+++ src/main/java/com/example/myblog/elasticsearch/mapper/SearchMapper.java
@@ -3,5 +3,8 @@ package com.example.myblog.elasticsearch.mapper;
 import com.example.myblog.elasticsearch.entity.IndexBlogEs;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+/**
+ * @author SJC
+ */
 public interface SearchMapper extends ElasticsearchRepository<IndexBlogEs,Long> {
 }
